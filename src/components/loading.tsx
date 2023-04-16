5d6efce3d1101ff092afdefd943b0218eb62534a
Audrow Nash <audrow@hey.com>
2023-04-16 16:54:10 -0500

    Add error message + loading + submit with enter

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,15 +1,15 @@
 export const LoadingPage = () => {
   return (
     <div className="absolute top-0 right-0 h-screen w-screen flex justify-center items-center">
-      <LoadingSpinner />
+      <LoadingSpinner size={40}/>
     </div>
   );
 };
 
-export const LoadingSpinner = () => {
+export const LoadingSpinner = ({size = 16}: {size: number}) => {
     return (
       <div
-        className="inline-block h-16 w-16 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+        className={`inline-block h-${size} w-${size} animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]`}
         role="status"
       >
         <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,9 @@ import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
-import { LoadingPage } from "@/components/loading";
+import { LoadingPage, LoadingSpinner } from "@/components/loading";
 import {useState} from "react";
+import { toast } from "react-hot-toast";
 
 dayjs.extend(relativeTime);
 
@@ -21,6 +22,14 @@ const CreatePostWizard = () => {
     onSuccess: () => {
       setInput("");
       void ctx.posts.getAll.invalidate()
+    },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error("Failed to post. Please try again later.");
+      }
     }
   });
 
@@ -28,10 +37,12 @@ const CreatePostWizard = () => {
 
   return (
     <div className="flex w-full gap-3">
-      <Image src={user.profileImageUrl} className="h-14 w-14 rounded-full" 
-      alt={`@${user.username} profile picture`}
-      width={56}
-      height={56}
+      <Image
+        src={user.profileImageUrl}
+        className="h-14 w-14 rounded-full"
+        alt={`@${user.username} profile picture`}
+        width={56}
+        height={56}
       />
       <input
         placeholder="Type some emojis!"
@@ -39,15 +50,31 @@ const CreatePostWizard = () => {
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            if (input !== "") {
+              mutate({ content: input });
+            }
+          }
+        }}
         disabled={isPosting}
       />
-      <button onClick={
-        () => {
-          mutate({content: input});
-        }
-      }>
-        Post
-      </button>
+      {input !== "" && !isPosting && (
+        <button
+          onClick={() => {
+            mutate({ content: input });
+          }}
+        >
+          Post
+        </button>
+      )}
+      {isPosting && (
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={16}/>
+        </div>
+      )
+    }
     </div>
   );
 }
@@ -96,7 +123,7 @@ const Feed = () => {
 
 
 const Home: NextPage = () => {
-  const {user, isLoaded: userLoaded, isSignedIn} = useUser();
+  const {isLoaded: userLoaded, isSignedIn} = useUser();
 
   // Start fetching posts ASAP (use cached data elsewhere)
   api.posts.getAll.useQuery();
